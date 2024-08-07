4344b725193a0f72b66bfc23b39cbc8397d07f9a
amlanxbghn <amlanxconnect@gmail.com>
2024-08-08 03:19:58 +0530

    Registration Page (coming soon)

--- DiversifyF/src/components/Register.jsx
+++ DiversifyF/src/components/Register.jsx
@@ -5,7 +5,7 @@ const Register = () => {
   return (
     <div className="flex flex-col items-center justify-center space-y-4">
       <h1 className="text-4xl font-bold">Registration Page will be available soon.</h1>
-      <p className="text-xl">Meanwhile, you can enjoy all the features without Registering. :)</p>
+      <p className="text-xl text-zinc-400">Meanwhile, you can enjoy all the features without Registering. :)</p>
       <Link to="/">
         <button className="px-4 py-2 bg-red-700 text-white rounded-lg hover:bg-red-800 duration-200">Back to Home</button>
       </Link>
